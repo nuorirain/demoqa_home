3bfd9e4dada4a419f8060f4e5df500b3ff600a09
nuorirain <seraser58snopdog@gmail.com>
2025-10-20 23:32:10 +0300

    base_component.py

---
+++ components/base_component.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+class BaseComponent:
+    def __init__(self, driver, locator):
+
+        self.driver = driver
+        self.locator = locator
+
+
+    def find_element(self):
+        return self.driver.find_element(By.CSS_SELECTOR, self.locator)
+
+    def get_text(self):
+
+        return str(self.find_element().text)
