5bca2bbde95c5a7da7647eed6232f03a95b4c33b
nuorirain <seraser58snopdog@gmail.com>
2025-10-20 23:22:03 +0300

    swag_labs.py

---
+++ pages/swag_labs.py
@@ -0,0 +1,29 @@
+from selenium.common.exceptions import NoSuchElementException
+from pages.base_page import BasePage
+
+
+class SwagLabs(BasePage):
+
+    
+    def exist_icon(self):
+        try:
+            self.find_element(locator='div.login_logo')  
+        except NoSuchElementException:
+            return False  
+        return True       
+
+ 
+    def exist_username_field(self):
+        try:
+            self.find_element(locator='input#user-name')
+        except NoSuchElementException:
+            return False
+        return True
+
+  
+    def exist_password_field(self):
+        try:
+            self.find_element(locator='input#password')
+        except NoSuchElementException:
+            return False
+        return True
