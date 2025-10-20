873734a8a02ffae156006838174b807e8c5cd9b9
nuorirain <seraser58snopdog@gmail.com>
2025-10-20 23:24:46 +0300

    base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,10 +2,7 @@ from selenium.webdriver.common.by import By
 
 class BasePage:
     def __init__(self, driver, base_url='https://www.saucedemo.com/'):
-        """
-        :param driver: объект webdriver
-        :param base_url: базовый URL страницы (по умолчанию Saucedemo)
-        """
+     
         self.driver = driver
         self.base_url = base_url
 
